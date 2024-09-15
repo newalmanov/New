d3c3bf11e7c31061bdc13abd0bf983de94daabbc
Homework <h.almanov@newuu.uz>
2024-09-15 10:13:03 +0500

    NewUU

---
+++ problem1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a=0,b=0,c=0;
+    cin>>a>>b>>c;
+    cout<<a-b-c<<endl;
+    cout<<b*c-a<<endl;
+    cout<<2*b-c+5*a<<endl;
+    return 0;
+}
