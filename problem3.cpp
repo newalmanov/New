6afc8676ccc0a1e4f5e2ac57f741d43b8652e1bc
newalmanov <h.almanov@newuu.uz>
2024-09-15 16:15:42 +0500

    Update problem3.cpp

--- problem3.cpp
+++ problem3.cpp
@@ -4,6 +4,6 @@ int main() {
     double C=0;
     cin>>C;
     double F=(C*9/5)+32;
-    cout<<"The temperature in Faranheit equal to "<<F<<endl;
+    cout<<"The temperature in Farenheit equal to "<<F<<endl;
     return 0;
 }
