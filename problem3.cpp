b8427e0ddc651f8caa75a725aff0c044d1ba1467
newalmanov <h.almanov@newuu.uz>
2024-09-15 16:19:08 +0500

    Update problem3.cpp

--- problem3.cpp
+++ problem3.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
-    double C=0;
+    float C=0;
     cin>>C;
-    double F=(C*9/5)+32;
+    float F=(C*9/5)+32;
     cout<<"The temperature in Farenheit equal to "<<F<<endl;
     return 0;
 }
